70f0ea579b7aa9e2798e666c9517c382c91d6f3f
vmdharan <rexar7@gmail.com>
2016-12-02 12:18:23 +1100

    Started work on a CSV to KML converter
    - Added a test CSV with sample coordinates.
    - Wrote a simple Python script to read the CSV and display the
    coordinates.

---
+++ csv_2_kml/Extract_LatLon.py
@@ -0,0 +1,23 @@
+# Extract_LatLon.py
+# Read the points.csv file which contains a list of coordinates in
+# (decimal format) latitude-longitude pairs. 
+
+sourceFile = open('points.csv', 'r')
+
+i = 0
+coordinates = sourceFile.readline()
+
+while(coordinates != ''):
+    print (coordinates)
+
+    latLonCoords = [latLon.strip() for latLon in coordinates.split(',')]
+    print (latLonCoords[0])
+    print (latLonCoords[1])
+
+    coordinates = sourceFile.readline()
+    
+    i += 1
+
+print (str(i) + ' coordinates read from file.')
+
+
